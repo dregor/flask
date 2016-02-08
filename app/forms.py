2a0da57b0b394e690daa29d4533caf1c2b4fc816
dregor614 <dregor614@bk.ru>
2016-02-08 17:02:37 +0300

    add registration

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,17 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
+from wtforms import TextField, BooleanField, PasswordField, HiddenField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
+    openid = HiddenField('openid')
+    login = TextField('login')
+    password = PasswordField('password')
     remember_me = BooleanField('remember_me', default = False)
+
+class RegisterForm(Form):
+    login = TextField('login')
+    email = TextField('email')
+    password = PasswordField('password')
+    password_confirm = PasswordField('password_confirm')
+
 
